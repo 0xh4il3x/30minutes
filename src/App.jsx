b7c21b923d6c9edca08534ac0b132ea1d92a07ae
0xh4il3x <haileamlaksahle@gmail.com>
2025-04-13 14:48:29 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+	<h1 className="h1tag">YOU ARE ABSOLUTELY COOKED. I AM SORRY. </h1>
+    </>
+  )
+}
+
+export default App
